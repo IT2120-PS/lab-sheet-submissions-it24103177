ecd9b9aab1b4030be4f449f730b30c5b820bc542
it24103177 <IT24103177@my.sliit.lk>
2025-10-08 22:15:45 +0530

    Add files via upload

---
+++ Lab Sheet 09/IT24103177.R
@@ -0,0 +1,8 @@
+setwd("C:\\Users\\Avishka\\Desktop\\IT24103177")
+getwd()
+
+#random sample of size 25 for the baking time
+sample <- rnorm(25, mean =45, sd =2)
+
+# Test whether the average baking time is less than 46 minutes at a 5% level of significance.
+t.test (sample, mu=46, alternative = "less")
